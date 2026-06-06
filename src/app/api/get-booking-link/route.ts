30da29344d8c39c19b018bd0b56ea96146e0c5d8
Codewizkashish <kashishnandwani1407@gmail.com>
2026-06-06 12:28:09 +0530

    fix: bookings

--- src/app/api/get-booking-link/route.ts
+++ src/app/api/get-booking-link/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   return NextResponse.json({
-    bookingLink: "https://cal.com/kashish-nandwani-funq0d/30min"
+    result: "https://cal.com/kashish-nandwani-funq0d/30min"
   });
 }

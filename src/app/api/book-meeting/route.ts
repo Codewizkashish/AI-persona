e045bbd28ea6d0a4c90d3a92958de7b907ebc679
Codewizkashish <kashishnandwani1407@gmail.com>
2026-06-06 13:00:59 +0530

    fix:book-meetings

---
+++ src/app/api/book-meeting/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const {
+      start,
+      name,
+      email,
+    } = await req.json();
+
+    const response = await fetch(
+      "https://api.cal.com/v2/bookings",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.CAL_API_KEY}`,
+          "Content-Type": "application/json",
+          "cal-api-version": "2024-09-04",
+        },
+        body: JSON.stringify({
+          start,
+          eventTypeSlug: "30min",
+          username: "kashish-nandwani-funq0d",
+          attendee: {
+            name,
+            email,
+            timeZone: "Asia/Kolkata",
+          },
+        }),
+      }
+    );
+
+    const data = await response.json();
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Booking failed" },
+      { status: 500 }
+    );
+  }
+}

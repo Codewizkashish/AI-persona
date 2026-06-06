3db6ed734eae65fc3287b4f764c6b5f13f376fa9
Codewizkashish <kashishnandwani1407@gmail.com>
2026-06-06 12:52:42 +0530

    fix: booking availability

--- src/app/api/check-availability/route.ts
+++ src/app/api/check-availability/route.ts
@@ -4,10 +4,11 @@ export async function POST(req: Request) {
   const { date } = await req.json();
 
   const response = await fetch(
-    `https://api.cal.com/v2/slots?eventTypeSlug=30min&start=${date}&end=${date}`,
+    `https://api.cal.com/v2/slots?eventTypeSlug=30min&username=kashish-nandwani-funq0d&start=${date}&end=${date}&format=range`,
     {
       headers: {
         Authorization: `Bearer ${process.env.CAL_API_KEY}`,
+        "cal-api-version": "2024-09-04",
       },
     }
   );
